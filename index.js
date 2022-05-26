46330636cbfabacfb34d404e41e02031c61fec9b
Billy <billypatrick767@gmail.com>
2022-05-26 12:03:02 +0300

    add: test start script

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Lets get started');
+const puppeteer = require("puppeteer");
+require("dotenv").config();
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(process.env.OPENMRS_BASE_URL);
+  await page.type("#username", process.env.OPENMRS_USER_NAME);
+  await page.type("#password", process.env.OPENMRS_USER_PASSWORD);
+  await page.waitForTimeout(500);
+  await page.click('input[type="submit"]');
+  await page.waitForTimeout(2000);
+  await page.goto(
+    "http://45.79.110.194:8081/openmrs/admin/locations/location.form"
+  );
+  await page.waitForTimeout(1000);
+
+  await browser.close();
+})();
